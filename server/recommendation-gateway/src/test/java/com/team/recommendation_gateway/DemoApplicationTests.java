fcb5ae674cca0910e29155aba1b5c792405fab59
Nabil Ben Jemia <nabilbenjemia01@gmail.com>
2025-05-27 20:59:53 +0200

    added recommendation-gateway directory

---
+++ server/recommendation-gateway/src/test/java/com/team/recommendation_gateway/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.recommendation_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
