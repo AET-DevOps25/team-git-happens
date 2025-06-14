a743fce4c365f965bae41162f9f1094b6a073647
aziz-bouziri <aziz.bouziri1@gmail.com>
2025-06-14 23:55:49 +0200

    remove comment

--- server/recommendation-gateway/src/main/java/com/team/recommendation_gateway/RecommendationController.java
+++ server/recommendation-gateway/src/main/java/com/team/recommendation_gateway/RecommendationController.java
@@ -1,6 +1,6 @@
 package com.team.recommendation_gateway;
 
-import org.springframework.beans.factory.annotation.Autowired; // NEU!
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
