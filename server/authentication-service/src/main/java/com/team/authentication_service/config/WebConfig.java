84341d8c2c6239b39deb081bdef0c97f0d06f0d2
Sina Mohammad Rezaei <mohammadrezaei@cip.ifi.lmu.de>
2025-07-02 23:27:02 +0200

    updated webconfig

--- server/authentication-service/src/main/java/com/team/authentication_service/config/WebConfig.java
+++ server/authentication-service/src/main/java/com/team/authentication_service/config/WebConfig.java
@@ -2,10 +2,12 @@ package com.team.authentication_service.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     private static final Logger logger = LoggerFactory.getLogger(WebConfig.class);
@@ -14,13 +16,12 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         logger.info("Registering CORS allowed origins");
         registry.addMapping("/**")
-            .allowedOriginPatterns(
-                "http://localhost:3000",
-                "http://client-app.student.k8s.aet.cit.tum.de",
-                "http://k83-client-app.student.k8s.aet.cit.tum.de"
-            )
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .allowedHeaders("Authorization", "Content-Type")
-            .allowCredentials(true);
+                .allowedOriginPatterns(
+                        "http://localhost:3000",
+                        "https://client-app.student.k8s.aet.cit.tum.de",
+                        "https://k83-client-app.student.k8s.aet.cit.tum.de")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Authorization", "Content-Type")
+                .allowCredentials(true);
     }
 }
