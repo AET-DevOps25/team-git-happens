750aed3fc42ee981e8386d6ee68fc4c8165a837d
Nabil Ben Jemia <nabilbenjemia01@gmail.com>
2025-07-08 12:41:37 +0200

    removed hardcoded credentials

--- server/mysql-init/01-init-databases.sql
+++ server/mysql-init/01-init-databases.sql
@@ -2,8 +2,9 @@ CREATE DATABASE IF NOT EXISTS course_db;
 CREATE DATABASE IF NOT EXISTS auth_db;
 CREATE DATABASE IF NOT EXISTS review_db;
 
-
-CREATE USER IF NOT EXISTS 'admin'@'%' IDENTIFIED BY 'githappens!';
+-- Create admin user with environment variable password
+-- This works in both Docker Compose and Kubernetes environments
+CREATE USER IF NOT EXISTS 'admin'@'%' IDENTIFIED BY '${MYSQL_ADMIN_PASSWORD}';
 GRANT ALL PRIVILEGES ON course_db.* TO 'admin'@'%';
 GRANT ALL PRIVILEGES ON auth_db.* TO 'admin'@'%';
 GRANT ALL PRIVILEGES ON review_db.* TO 'admin'@'%';
